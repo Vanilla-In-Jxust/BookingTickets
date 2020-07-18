a0638b75c6b196c2f770607c86ea1c04280784e6
VergeDX <wxy20001225@126.com>
2020-07-18 11:05:55 +0800

    [+] bookTicket() method

--- src/Validate.h
+++ src/Validate.h
@@ -15,6 +15,13 @@ using namespace std;
 regex timeRegex = regex("^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$");
 regex integerRegex = regex("[0-9]+");
 regex doubleRegex = regex("[0-9]*\\.[0-9]+|[0-9]+");
+regex chinaLongIdRegex = regex("^\\d{15}|\\d{18}$");
+
+/**
+ *  or "^\d{8,18}|[0-9x]{8,18}|[0-9X]{8,18}?$",
+ *  see also https://tojohnonly.github.io/63-C++%E4%BD%BF%E7%94%A8%E6%AD%A3%E5%88%99%E8%A1%A8%E8%BE%BE%E5%BC%8F.html
+ */
+regex chinaShortIdRegex = regex("^([0-9]){7,18}(x|X)?$");
 
 bool isTime(const string &timeString) {
     return regex_match(timeString, timeRegex);
@@ -26,4 +33,10 @@ bool isPositiveInteger(const string &integerString) {
 
 bool isPositiveDouble(const string &doubleString) {
     return regex_match(doubleString, doubleRegex);
+}
+
+bool isChinaId(const string &idString) {
+    // see also https://my.oschina.net/anziguoer/blog/1780511
+    return regex_match(idString, chinaLongIdRegex) ||
+           regex_match(idString, chinaShortIdRegex);
 }
